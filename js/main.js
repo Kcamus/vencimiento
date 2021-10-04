c48c168e8aa2e5017ad719952ffaf6cbbb0cc401
luis pacheco <laps1308@gmail.com>
2021-10-04 09:28:32 -0500

    se corrige btn en Home.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+
+
+let btn_login= document.getElementById("btn_login");
+btn_login.onclick=login
+
+
+
 function redirectTo(){
 
   if(event.target.id=="registro"){
@@ -7,4 +15,27 @@ function redirectTo(){
   else if(event.target.id=="consulta"){
     console.log(window.location.assign(/*window.location.host+*/"./Consulta.html"))
   }
+}
+
+function login(){
+ let user,password,solicitud;
+ user=document.getElementById('User').value;
+ password=document.getElementById('Password').value;
+ if (user==null || password== null){
+   alert("Por favor ingrese usuario y contraseña,estos son requeridos");
+ }
+ 
+ else{
+   try {
+
+     solicitud= new XMLHttpRequest();
+     solicitud.onload= ()=>{
+       
+     }
+     
+   } catch (error) {
+     
+   }
+ }
+
 }
---
+++ src/login.php
@@ -0,0 +1,4 @@
+<?php
+$user= $_POST['User'];
+$password=$_POST['Passowrd'];
+return "usuario ${user} y contraseña ${contraseña}";
